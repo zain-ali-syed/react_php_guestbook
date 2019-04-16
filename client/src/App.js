1d29c9854d495e1e6a34d5623989e2c3a0bbcf5d
messagetobeats <chelnov63@hotmail.com>
2019-04-16 10:53:05 +0100

    logo added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Messages from './components/Messages';
+import Logo from './images/logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h3 className="title"> React-PHP-mySQL guestbook </h3>
+        <div className="center-align">
+          <img src={Logo} alt="logo" />
+        </div>
         <Messages />
       </div>
     );
